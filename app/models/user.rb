100512d1f9b6ab27396dab0d70ba6e4414670eac
James Fox <jcfox412@gmail.com>
2013-04-23 04:04:58 -0400

    Update user with new associations, add fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :about, :password, :password_confirmation
   has_secure_password
+
+  has_many :listings
+  has_many :messages
+  has_one :image, :as => :imageable
 
   validates_confirmation_of :email
   validates_presence_of :password, :on => :create
