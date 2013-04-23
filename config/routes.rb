942a97719ebf84b7c0c50877d8997f24aac20c1a
James Fox <jcfox412@gmail.com>
2013-04-23 01:18:51 -0400

    Set up basic user login system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Teamchill::Application.routes.draw do
+
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  
+  resources :users
+  resources :sessions
+
+  root :to => "sessions#new"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
