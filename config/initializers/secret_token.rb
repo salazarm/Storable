8dea6aebf1682ffd5369ca830c76c3e4cb910868
unknown <Jamie@Jamie-PC.(none)>
2013-04-23 00:51:18 -0400

    Add basic rails app framework. -jcfox

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamchill::Application.config.secret_token = '3866a0e96bd9ada69a06e52dc29b84035f048fa75dc9e756aa07113bc04346dfd77445bf5ad38b35a49003c954efd0f306a841f9514163b8f9a77d2128134382'
