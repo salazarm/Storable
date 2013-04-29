efadd1cc467bf644b585c04064f1045603405934
Marco Salazar <salazarm@mit.edu>
2013-04-29 02:58:56 -0400

    premerge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
 			return 0
 		end
 	end
+
+	def profile_picture_for(user)
+		return user.images.last ? user.images.last.location : user.photo
+	end
+
+	def pretty_name(user)
+		user.first_name.nil? ? user.email : user.first_name + " " + user.last_name
+	end
 end
