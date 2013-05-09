5817812b8c9a939a137debb6d7981afab88ad4b5
Marco Salazar <salazarm@mit.edu>
2013-05-09 02:00:42 -0400

    t

--- app/models/review (adphi-winath's conflicted copy 2013-05-08).rb
+++
@@ -1,8 +0,0 @@
-class Review < ActiveRecord::Base
-	attr_accessible :reviewer_id, :content, :overall_rating, :quality_rating, 
-		:transaction_id, :location_rating, :price_rating,
-		:reviewee_id, :timeliness_rating, :responsiveness_rating
-
-		belongs_to :reviewer, :class_name => "User", :foreign_key => "reviewer_id"
-    belongs_to :listing, :class_name => "Listing", :foreign_key => "listing_id"
-end
